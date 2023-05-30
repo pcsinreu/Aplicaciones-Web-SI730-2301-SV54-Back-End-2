7095e6138567c753476a35f132aee4f2c1641664
nreupomckinsey <109175146+nreupomckinsey@users.noreply.github.com>
2023-05-29 21:02:47 -0500

    Update unit Test

---
+++ LearningCenter.Domain.Test/TesteableDomainUnitTest.cs
@@ -0,0 +1,22 @@
+namespace LearningCenter.Domain.Test;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(10,20, 30)]
+    [InlineData(10,100, 110)]
+    [InlineData(20,20, 40)]
+    public void sum_integerValues_ReturnSum(int numberA,int numberB,int expected)
+    {
+        //Arrange - setup
+        TesteableClass testeableClass = new TesteableClass();
+        
+        //Act - exce
+        int result = testeableClass.sum(numberA, numberB);
+
+        
+        //Assert
+        Assert.Equal(result,expected);
+        
+    }
+}
